a1ddbfdf8c52c2e860bb1b336fe6177967636b42
ratnak1990 <ratnak1990@gmail.com>
2016-03-22 12:05:02 +0530

    submitting assignment

---
+++ ACD_FE_Session_2_Assignment_3_Main.js
@@ -0,0 +1,10 @@
+var person={
+    firstName:"Aamir",
+    lastName:"Khan",
+    email:"aamir.khan@gmail.com",
+    phone:"02228490838"
+}
+document.getElementById("fname").innerHTML=person.firstName +"&nbsp" ;
+document.getElementById("lname").innerHTML=person.lastName +"&nbsp" ;
+document.getElementById("email").innerHTML=person.email +"&nbsp";
+document.getElementById("phone").innerHTML=person.phone +"&nbsp";
